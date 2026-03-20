ad49b6c2271916a9d1c052e03f9ac894ce37ed51
Ankur <churasiyaankur8871@gmail.com>
2026-03-21 00:33:32 +0530

    Added New pages

--- assets/js/main.js
+++ assets/js/main.js
@@ -32,6 +32,26 @@ $(document).ready(function () {
     });
 });
 
+// Awards Slider ( awards-slider-one)
+
+$(document).ready(function () {
+    var swiper = new Swiper('.awards-slider-one', {
+        loop: true,
+        speed: 800,
+        autoplay: {
+            delay: 3000,
+            disableOnInteraction: false,
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        effect: 'slide',
+    });
+});
+
+
+
 
 // Navigation Script
 jQuery(document).ready(function($) {
@@ -40,4 +60,34 @@ jQuery(document).ready(function($) {
         position: 'right',
     });
 });
+
+// Read More Button
+document.querySelectorAll('.member-content').forEach(function(card) {
+    const desc = card.querySelector('.member-description');
+    const btn = card.querySelector('.member-read-more-btn');
+
+    const fullText = desc.innerText;
+    const words = fullText.split(' ');
+
+    if (words.length > 30) {
+        const shortText = words.slice(0, 40).join(' ') + '...';
+        desc.innerText = shortText;
+
+        let expanded = false;
+
+        btn.addEventListener('click', function() {
+            if (!expanded) {
+                desc.innerText = fullText;
+                btn.innerText = "Read Less";
+            } else {
+                desc.innerText = shortText;
+                btn.innerText = "Read More";
+            }
+            expanded = !expanded;
+        });
+    } else {
+        btn.style.display = "none";
+    }
+});
+    
 	
